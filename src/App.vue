267ccb81dd187543c63e8ce17448a36c7bdf9a5b
John Doe <kurbanof.atabek@gmail.com>
2021-10-17 15:59:46 +0600

    basic setup

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'SF Pro Display',Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
